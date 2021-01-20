4025b77a946b1f4a4a675bbce63cdd40c039cb86
Swagata Roy <slimeeunity@gmail.com>
2021-01-20 12:28:51 +0530

    Added Project Feed

--- BaseApp/models.py
+++ BaseApp/models.py
@@ -5,8 +5,8 @@ from django.db import models
 class Project(models.Model):
 	project_name = models.CharField(max_length=50)
 	project_desc = models.TextField()
-	github_link = models.CharField(max_length=250,null=True)
-	demo_file = models.FileField(upload_to='demos',null=True)
-	live_url = models.CharField(max_length=250)
+	github_link = models.CharField(max_length=250,null=True,blank=True)
+	demo_file = models.FileField(upload_to='demos',null=True,blank=True)
+	live_url = models.CharField(max_length=250,blank=True)
 	def __str__(self):
 		return f'{self.project_name}'
